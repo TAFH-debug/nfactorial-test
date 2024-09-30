a7d679a6abb93f2b9fe0f40eda8c28795d7c8e4c
useing123 <useing@mail.ru>
2024-09-30 18:43:32 +0500

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.facebook.com'], // Add any external domains if needed
+    formats: ['image/avif', 'image/webp'], // If you want to support specific image formats
+  },
 };
 
 export default nextConfig;
