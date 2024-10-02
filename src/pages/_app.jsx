0d264566a26b2bcc54e4c8fe56c9eb3766370332
useing123 <useing@mail.ru>
2024-10-02 16:59:12 +0500

    added google tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,16 +2,23 @@
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  <Script id="G-WY8EBLD4VH" strategy="afterInteractive">
-  {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <>
+    <Script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-WY8EBLD4VH"
+    />
+    ;
+    <Script id="G-WY8EBLD4VH" strategy="afterInteractive">
+      {`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-WY8EBLD4VH');
-  `}
-</Script>
-  return <Component {...pageProps} />;
+gtag('config', 'G-WY8EBLD4VH');
+`}
+    </Script>
+    ; return <Component {...pageProps} />;
+  </>;
 }
 
 export default MyApp;
