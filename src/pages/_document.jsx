b0f8b0d909a10f8853af7efa1742aee6d186d4b8
useing123 <useing@mail.ru>
2024-10-02 18:31:56 +0500

    added gtm

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,69 +1,30 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import Image from "next/image";
-// import CookieConsent from '../components/CookieConsent';
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/* Подключение Google Fonts */}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-
-          {/* Код Meta Pixel */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '827157661462061');
-                fbq('track', 'PageView');
-              `,
-            }}
-          />
-          <noscript>
-            <Image
-              src="https://www.facebook.com/tr?id=827157661462061&ev=PageView&noscript=1"
-              alt="Facebook Pixel"
-              width={1}
-              height={1}
-            />
-          </noscript>
-
-          {/* Подключение Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-WY8EBLD4VH`}  // Замените на ваш идентификатор Google Analytics
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-WY8EBLD4VH', {  // Замените на ваш идентификатор Google Analytics
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        {/* Google Analytics tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WY8EBLD4VH"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WY8EBLD4VH');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
