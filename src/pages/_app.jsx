6d892ad45c295641b25d9180131b05d1a1dedae8
useing123 <useing@mail.ru>
2024-10-03 18:27:49 +0500

    reimport pixel

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,12 @@
-import { GoogleAnalytics } from '@next/third-parties/google'
- 
+import { GoogleAnalytics } from "@next/third-parties/google";
+import MetaPixel from "@next/third-parties/google/MetaPixel";
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <GoogleAnalytics gaId="G-WY8EBLD4VH" />
+      <MetaPixel pixelId="827157661462061" />
     </>
-  )
+  );
 }
