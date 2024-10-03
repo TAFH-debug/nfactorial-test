719629386b9ff8f1749311e31202bd22d4f22e44
useing123 <useing@mail.ru>
2024-10-03 21:13:06 +0500

    added pixel

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,10 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
-import MetaPixel from "@next/third-parties/google/MetaPixel";
-
+import { GoogleAnalytics } from '@next/third-parties/google'
+ 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <GoogleAnalytics gaId="G-WY8EBLD4VH" />
-      <MetaPixel pixelId="827157661462061" />
     </>
-  );
+  )
 }
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -15,6 +15,31 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          {/* Meta Pixel Code */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '827157661462061');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <Image 
+              src="https://www.facebook.com/tr?id=827157661462061&ev=PageView&noscript=1" 
+              alt="Facebook Pixel" 
+              width={1} 
+              height={1} 
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
