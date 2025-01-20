a6aa8b22e5f8aa8554125b9c5a9bf5ed12403d80
useing123 <useing@mail.ru>
2025-01-20 20:58:59 +0500

    added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,21 @@
 import { GoogleAnalytics } from '@next/third-parties/google'
- 
+import posthog from 'posthog-js';
+
+
 export default function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_API_KEY) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+        autocapture: true, // Автоматический сбор событий
+      });
+
+      // Опционально: Отправка кастомного события
+      posthog.capture('page_loaded', { path: window.location.pathname });
+    }
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
